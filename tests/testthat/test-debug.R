dd3b3af755390f7a86ec12dd0e8ea788bc70cd20
Gábor Csárdi <csardi.gabor@gmail.com>
2016-11-01 03:09:30 +0000

    More test cases

--- tests/testthat/test-debug.R
+++ tests/testthat/test-debug.R
@@ -7,3 +7,44 @@ test_that("debug prints", {
     "pkg foobar"
   )
 })
+
+test_that("format_date", {
+  d <- "2016-11-01 02:33:54"
+  expect_identical(
+    format_date(d),
+    "2016-11-01T02:33:54.54.000+00:00"
+  )
+})
+
+test_that("get_timestamp_stamp", {
+
+  with_mock(
+    `base::Sys.time` = function() as.POSIXct("2016-11-01 02:33:54"),
+    expect_equal(
+      get_timestamp_stamp(),
+      "2016-11-01T02:33:54.54.000+00:00 "
+    ),
+    expect_equal(
+      get_timestamp(mode = "stamp"),
+      "2016-11-01T02:33:54.54.000+00:00 "
+    )
+  )
+})
+
+test_that("debugging to a file", {
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+
+  on.exit(initialize_output_file(), add = TRUE)
+  on.exit(try(close(debug_data$output_fd), silent = TRUE), add = TRUE)
+
+  withr::with_envvar(c(DEBUGME_OUTPUT_FILE = tmp), {
+    initialize_output_file()
+  })
+  debug("hello world!", "foobar")
+  debug("hello again!", "foo")
+
+  log <- readLines(tmp)
+  expect_match(log[1], "^foobar .*hello world!$")
+  expect_match(log[2], "^foo .*hello again!$")
+})
---
+++ tests/testthat/test-dynamic.R
@@ -0,0 +1,15 @@
+
+context("dynamic messages")
+
+test_that("handle_dynamic_code", {
+
+  expect_equal(
+    handle_dynamic_code("blah"),
+    "blah"
+  )
+
+  expect_equal(
+    handle_dynamic_code("blah `x` borg"),
+    substitute(paste0("blah ", x, " borg"))
+  )
+})
